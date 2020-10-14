c7444cd75639f139e7cbbae2498b2e78f95ea9fa
Patricia Rodrigues Alves <rpatricia@gmail.com>
2020-10-14 15:07:15 -0300

    Adding Page 2

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,33 @@
+//setView([latitude, longitude],zoom)
+//Create map
+const map = L.map('mapid').setView([-23.5835824,-46.5931261], 17);
+
+
+//Create icon
+const icon = L.icon({
+    iconUrl: "/public/images/map-marker.svg",
+    iconSize: [58,68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+//Create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Casa Madre Assunta Marchetti <a href="orphanage.html?id=1" class="choose-orphanage"><img src="public/images/arrow-white.svg"></a>')
+
+
+//create and add TileLayer
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    ).addTo(map);
+
+
+    //Create and add marker
+L.marker([-23.5835824,-46.5931261],{icon:icon})
+    .addTo(map)
+    .bindPopup(popup)
+
